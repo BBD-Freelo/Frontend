3de5a947db05bd9564cb1f8651516eb902c1746f
AdrianHawkins <Adrian.Hawkins@bbd.co.za>
2024-05-24 21:54:30 +0200

    environment variables

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
-console.log(process.env["sdfjsd"])
+import { environment} from "./environments/environment";
+console.log(environment.api_url);
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
