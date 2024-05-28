9dedc7ef5b2c32aa3145bd7de3dfa83813a49804
AdrianHawkins <Adrian.Hawkins@bbd.co.za>
2024-05-28 10:32:53 +0200

    interface updates to match backend

--- src/app/interfaces/entities/ticket.d.ts
+++ src/app/interfaces/entities/ticket.d.ts
@@ -3,6 +3,7 @@ import {User} from "./user";
 export interface Ticket {
   ticketId: number;
   user: User;
+  assignedUser: User | null;
   ticketName: string;
   ticketDescription: string;
   ticketCreateDate: string;
