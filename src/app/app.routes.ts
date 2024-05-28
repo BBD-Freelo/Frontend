570ca3c3b1fb2d0d342fab28939aa6f6c9305ebc
Janro Botha <Janro.Botha@bbd.co.za>
2024-05-28 14:26:43 +0200

    signup page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,8 @@ import { Routes } from '@angular/router';
 import { BoardComponent } from './pages/board/board.component';
 import { HomeComponent } from "./pages/home/home.component";
 import { authGuard } from './auth.guard';
+import { SignupComponent } from './components/signup/signup.component';
+import { ConfirmSignupComponent } from './components/confirm-signup/confirm-signup.component';
 
 export const routes: Routes = [
   {
@@ -14,5 +16,7 @@ export const routes: Routes = [
     loadComponent: () =>
       import('./pages/board/board.component').then(m => m.BoardComponent),
     canActivate: [authGuard]
-  }
+  },
+  { path: 'confirm-signup', component: ConfirmSignupComponent },
+  { path: 'signup', component: SignupComponent },
 ];
