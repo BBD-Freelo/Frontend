f4f57743bbe0951dbaf52e73784b3316f6902f17
GerniVisser-bbd <Gerrit.Visser@bbd.co.za>
2024-05-29 22:08:43 +0200

    added delete modals and input modals

---
+++ src/app/components/delete-dialog/delete-dialog.component.ts
@@ -0,0 +1,34 @@
+import { Component, EventEmitter, Inject, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MAT_DIALOG_DATA, MatDialogActions, MatDialogClose, MatDialogContent, MatDialogRef, MatDialogTitle } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { DialogData } from '../../interfaces/components/dialogData';
+
+@Component({
+  selector: 'app-delete-dialog',
+  standalone: true,
+  imports: [
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    MatButtonModule,
+    MatDialogTitle,
+    MatDialogContent,
+    MatDialogActions,
+    MatDialogClose,
+  ],
+  templateUrl: './delete-dialog.component.html',
+  styleUrl: './delete-dialog.component.css'
+})
+export class DeleteDialogComponent {
+  constructor(
+    public dialogRef: MatDialogRef<DeleteDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: DialogData,
+  ) {}
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+}
