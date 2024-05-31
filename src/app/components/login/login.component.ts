c165530921fe6da85cb03cd491d748b53d196cc5
Lucinda Jordaan <Lucinda.Botes@bbd.co.za>
2024-05-31 09:35:35 +0200

    fix: Update to @if for errors in login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,20 +1,24 @@
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormControl, FormsModule, Validators, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { signIn } from 'aws-amplify/auth';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatLabel } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { Regex } from '../../../enums/regex';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule, FormsModule, MatButtonModule, MatFormFieldModule, MatInputModule, MatLabel],
+  imports: [CommonModule, FormsModule, MatButtonModule, MatFormFieldModule, MatInputModule, MatLabel, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  passwordFormControl = new FormControl('', [Validators.required, Validators.minLength(8)]);
+  emailFormControl = new FormControl('', [Validators.required, Validators.pattern(Regex.Email)]);
+  loginError: string | null = null;
 
   email: string = '';
   password: string = '';
@@ -24,14 +28,18 @@ export class LoginComponent {
   }
 
   async signInUser(){
+    this.loginError = null;
+    if (this.passwordFormControl.invalid || this.emailFormControl.invalid) {
+      return;
+    };
     try {
       await signIn({
         username: this.email,
         password: this.password
       });
       console.log("Login successful");
-    } catch (err) {
-      console.log("Error signing In : ", err);
+    } catch (err: any) {
+      this.loginError = err.message;
     }
   }
 
