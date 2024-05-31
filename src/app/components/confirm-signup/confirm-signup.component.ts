3a105cc11ab7b060d585d8ef46634d252ad7e169
Lucinda Jordaan <Lucinda.Botes@bbd.co.za>
2024-05-31 09:42:30 +0200

    fix: Update to @id for confirm signup errors

--- src/app/components/confirm-signup/confirm-signup.component.ts
+++ src/app/components/confirm-signup/confirm-signup.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { confirmSignUp, ConfirmSignUpInput } from 'aws-amplify/auth';
-import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { FormControl, FormsModule, ReactiveFormsModule, Validators } from "@angular/forms";
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatLabel } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { Regex } from '../../../enums/regex';
 
 @Component({
   selector: 'app-confirm-signup',
@@ -14,7 +15,14 @@ import { MatInputModule } from '@angular/material/input';
   styleUrl: './confirm-signup.component.css'
 })
 export class ConfirmSignupComponent {
+  emailFormControl = new FormControl('', [Validators.required, Validators.pattern(Regex.Email)]);
+  codeFormControl = new FormControl('', [Validators.required]);
+  confirmationError: string | null = null;
   async onSubmit(form: any) {
+    this.confirmationError = null;
+    if (this.codeFormControl.invalid || this.emailFormControl.invalid) {
+      return;
+    };
     const { email, code } = form.value;
   
     let user : ConfirmSignUpInput = {
@@ -25,8 +33,8 @@ export class ConfirmSignupComponent {
     try {
       const output = await confirmSignUp(user);
       console.log('Confirmation success!', output);
-    } catch (error) {
-      console.error('Error confirming sign up:', error);
+    } catch (error: any) {
+      this.confirmationError = error.message;
     }
   }
 }
