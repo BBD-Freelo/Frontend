8dd9b74346e8ca24982f32830436778bf38f26dd
GerniVisser-bbd <Gerrit.Visser@bbd.co.za>
2024-06-03 20:16:35 +0200

    routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,23 +4,13 @@ import { HomeComponent } from "./pages/home/home.component";
 import { authGuard } from './auth.guard';
 import { SignupComponent } from './components/signup/signup.component';
 import { ConfirmSignupComponent } from './components/confirm-signup/confirm-signup.component';
-import { UserprofileComponent } from './components/userprofile/userprofile.component';
 import { LoginComponent } from './components/login/login.component';
 
 export const routes: Routes = [
-  {
-    path: '',
-    loadComponent: () =>
-      import('./components/login/login.component').then(m => m.LoginComponent)
-  },
-  {
-    path: 'board/:board',
-    loadComponent: () =>
-      import('./pages/board/board.component').then(m => m.BoardComponent),
-    canActivate: [authGuard]
-  },
+  { path: 'login', component: LoginComponent},
+  { path: 'board/:board', component: BoardComponent, canActivate: [authGuard] },
   { path: 'confirm-signup', component: ConfirmSignupComponent },
   { path: 'signup', component: SignupComponent },
-  // { path: 'profile', component: UserprofileComponent },
   { path: 'login', component: LoginComponent },
+  { path: '**', redirectTo: 'login' }
 ];
