32244af2228ef8905db042e132f064450c8a8eec
Janro Botha <Janro.Botha@bbd.co.za>
2024-06-03 08:09:57 +0200

    Logout

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { MatToolbar } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { signIn, signOut } from 'aws-amplify/auth';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -11,5 +13,19 @@ import { MatIconModule } from '@angular/material/icon';
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+
+  constructor(private router: Router) {}
+
+
+  async signOutUser() {
+    try {
+      await signOut();
+      console.log('Sign out success!');
+      this.router.navigate(['/login']);
+    } catch (error) {
+      console.error('Error signing out:', error);
+    }
+  }
+
 
 }
