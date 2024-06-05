686d12de1aa9588786d0e8ea2826e534460800f5
AdrianHawkins <Adrian.Hawkins@bbd.co.za>
2024-06-05 11:08:58 +0200

    routes lazy loaded

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,4 @@
 import { Routes } from '@angular/router';
-import { authGuard } from './auth.guard';
-import { SignupComponent } from './components/signup/signup.component';
-import { ConfirmSignupComponent } from './components/confirm-signup/confirm-signup.component';
-import { LoginComponent } from './components/login/login.component';
 
 export const routes: Routes = [
   {
@@ -13,10 +9,21 @@ export const routes: Routes = [
   {
     path: 'board/:board',
     loadComponent: () =>
-      import('./pages/home/home.component').then(m => m.HomeComponent),
-    canActivate: [authGuard]
+      import('./pages/home/home.component').then(m => m.HomeComponent)
   },
-  { path: 'confirm-signup', component: ConfirmSignupComponent },
-  { path: 'signup', component: SignupComponent },
-  { path: 'login', component: LoginComponent },
+  {
+    path: 'confirm-signup',
+    loadComponent: () =>
+      import('./components/confirm-signup/confirm-signup.component').then(m => m.ConfirmSignupComponent)
+  },
+  {
+    path: 'signup',
+    loadComponent: () =>
+      import('./components/signup/signup.component').then(m => m.SignupComponent)
+  },
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('./components/login/login.component').then(m => m.LoginComponent)
+  },
 ];
